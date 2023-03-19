d3f31512c8d927b3b1ea51b6f6fda2828af8ce13
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-03-19 20:17:47 +0100

    search works

--- client/src/components/Main/Courses/ListCourses/ListCourses.jsx
+++ client/src/components/Main/Courses/ListCourses/ListCourses.jsx
@@ -1,3 +1,28 @@
+import { React, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import CardCourse from "./CardCourse/CardCourse";
+
+const ListCourses = (props) => {
+  const [search, setSearch] = useState("");
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+  
+  return (
+    <section>
+      <input type="search" name="search" onChange={onChange} placeholder="Search" />
+      <h1>Cursos</h1>
+      {search
+        ? props.data.filter((course) => course.course_title && course.course_title.toLowerCase().includes(search.toLowerCase()))
+            .map((course) => <CardCourse data={course} key={uuidv4()} />)
+        : props.data.map((course) => <CardCourse data={course} key={uuidv4()} />)
+      }
+    </section>
+  );
+};
+export default ListCourses;
+
+/* 
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 import CardCourse from "./CardCourse/CardCourse";
@@ -9,4 +34,4 @@ const ListCourses = (props) => {
   </section>;
 };
 
-export default ListCourses;
+export default ListCourses; */
