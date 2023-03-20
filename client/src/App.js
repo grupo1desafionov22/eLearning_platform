3d958072e2e2516914776e9bf140b789ff9c3445
Cesario87 <ces.albred@gmail.com>
2023-03-20 20:02:03 +0100

    dummy commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
-=======
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
->>>>>>> develop
 
 import Head from './components/Head';
 import AdminHead from './components/AdminHead';
 import Head2 from './components/Main/Home/Head2';
 import Main from './components/Main';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-import { BrowserRouter} from 'react-router-dom';
-=======
->>>>>>> develop
 
 function App() {
   const [user, setUser] = useState({});
@@ -26,10 +19,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-<<<<<<< HEAD
- 
         <Head />
-=======
         <Routes>
           <Route path="/admin/*" element={<AdminHead />} />
           <Route path="/*" element={<>
@@ -37,7 +27,6 @@ function App() {
             <Head2 />
           </>} />
         </Routes>
->>>>>>> develop
         <Main />
       </BrowserRouter>
       <Footer />
