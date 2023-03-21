7dd4545b29b7c4ac9459230bec650339947d4a31
Cesario87 <ces.albred@gmail.com>
2023-03-21 14:46:29 +0100

    dummy5

--- client/src/components/Head/Nav/Nav.jsx
+++ client/src/components/Head/Nav/Nav.jsx
@@ -1,29 +1,22 @@
-<<<<<<< HEAD
-import { React } from "react";
-=======
->>>>>>> develop
 
 import React from "react";
-import {  useState } from 'react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { Link } from "react-router-dom";
-import {  onLogout } from '../../../api/auth'
+import { onLogout } from '../../../api/auth'
 import { unauthenticateUser } from '../../../redux/slices/authSlice'
 import Login from '../../Main/Login/Login'
+
 const Nav = () => {
-<<<<<<< HEAD
-  
-=======
 
   const dispatch = useDispatch()
-/*   const [loading, setLoading] = useState(true)
-  const [protectedData, setProtectedData] = useState(null) */
+  /*   const [loading, setLoading] = useState(true)
+    const [protectedData, setProtectedData] = useState(null) */
 
   const [search, setSearch] = useState("");
   const onChange = (e) => {
     setSearch(e.target.value);
   };
->>>>>>> develop
 
   const logout = async () => {
     try {
@@ -37,33 +30,22 @@ const Nav = () => {
       console.log(error.response)
     }
   }
- 
-     return <nav className={"nav-bar"}>
-  <div>
-    <img src="" alt={"Logo"} />
-  </div>
-  <div>
-<<<<<<< HEAD
-        <a href={"ruta-de-inicio-de-sesion"}>Login</a>
-=======
-  <input type="search" name="search" onChange={onChange} placeholder="Search" />
-  </div>
-  <div>
-        <Link to="/login">Acceder</Link>
-        <Link to="/register">Sign up</Link>
->>>>>>> develop
-  </div>
+
+  return <nav className={"nav-bar"}>
+    <div>
+      <img src="" alt={"Logo"} />
+    </div>
+    <div>
+      <input type="search" name="search" onChange={onChange} placeholder="Search" />
+    </div>
+    <div>
+      <Link to="/login">Acceder</Link>
+      <Link to="/register">Sign up</Link>
+    </div>
 
 
-<button onClick={() => logout()} className='btn btn-primary'>Logout</button>
+    <button onClick={() => logout()} className='btn btn-primary'>Logout</button>
   </nav>;
-
-
-
-
-
-
-
 };
 
 export default Nav;
--- client/src/components/Main/Home/Head2/Nav2/Nav2.jsx
+++ client/src/components/Main/Home/Head2/Nav2/Nav2.jsx
@@ -3,13 +3,6 @@ import { Link } from "react-router-dom";
 import './Nav2.css'
 
 const Nav2 = () => {
-<<<<<<< HEAD
-  return <nav className={"nav-bar"}>
-  <Link to="/courses" className="buttonCard"><p className="filter">Cursos</p></Link>
-  <Link to="/comunidad" className="buttonCard"><p className="filter">Comunidad</p></Link>
-  <Link to="/explora" className="buttonCard"><p className="filter">Explora</p></Link> 
-</nav>;
-=======
 
   return (
     <nav className={"nav-bar"}>
@@ -21,7 +14,6 @@ const Nav2 = () => {
       </button>
     </nav>
   );
->>>>>>> develop
 };
 
 export default Nav2;
