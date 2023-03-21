e065c70276614c37520f9a35058e8b91cb1744a4
Gabriela Garcia Serrano <118684159+GabrielaSpace@users.noreply.github.com>
2023-03-21 14:17:47 +0100

    styles comunidad, cursos

--- client/src/components/Head/Nav/Nav.jsx
+++ client/src/components/Head/Nav/Nav.jsx
@@ -1,17 +1,11 @@
-import { React, useState } from "react";
+import { React } from "react";
 
 const Nav = () => {
-  const [search, setSearch] = useState("");
-  const onChange = (e) => {
-    setSearch(e.target.value);
-  };
+  
 
   return <nav className={"nav-bar"}>
   <div>
     <img src="" alt={"Logo"} />
-  </div>
-  <div>
-  <input type="search" name="search" onChange={onChange} placeholder="Search" />
   </div>
   <div>
         <a href={"ruta-de-inicio-de-sesion"}>Login</a>
--- client/src/components/Main/Home/Head2/Nav2/Nav2.jsx
+++ client/src/components/Main/Home/Head2/Nav2/Nav2.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Nav2.css'
 
 const Nav2 = () => {
   return <nav className={"nav-bar"}>
-  <Link to="/courses">Cursos</Link>
-  <Link to="/comunidad">Comunidad</Link>
-  <Link to="">Explora</Link> 
+  <Link to="/courses" className="buttonCard"><p className="filter">Cursos</p></Link>
+  <Link to="/comunidad" className="buttonCard"><p className="filter">Comunidad</p></Link>
+  <Link to="/explora" className="buttonCard"><p className="filter">Explora</p></Link> 
 </nav>;
 };
 
